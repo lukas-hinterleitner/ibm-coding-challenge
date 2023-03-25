eb5e2b20e66381722f623360ba1efa3a399431fa
Lukas Hinterleitner <lukas.hinterleitner@innotechnologies.at>
2023-03-25 12:39:14 +0100

    setup test environment using junit5

---
+++ src/test/java/org/example/test/ElevatorControllerTests.java
@@ -0,0 +1,12 @@
+package org.example.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ElevatorControllerTests {
+
+    @Test
+    public void TestEnvironmentWorks() {
+        Assertions.assertTrue(true);
+    }
+}
